9126678ea839a167369aa7579f8cb4ceb0897fb9
Vinay <Vinay.Misra@kyndryl.com>
2025-07-18 10:40:13 +0200

    added app files

---
+++ app.py
@@ -0,0 +1,21 @@
+
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+
+trace.set_tracer_provider(TracerProvider())
+tracer = trace.get_tracer(__name__)
+
+otlp_exporter = OTLPSpanExporter(
+    endpoint="http://elastic-apm-server.elastic-system.svc.cluster.local:8200",  # Replace with actual APM server URL
+    headers={"Authorization": "Bearer SSXSDGSptEv05J0u67E5ew"}
+)
+
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(otlp_exporter)
+)
+
+with tracer.start_as_current_span("minikube-span"):
+    print("Hello from Minikube!")
+
