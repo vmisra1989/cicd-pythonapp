337a39ca1de2909a88ed839514890f1271b911ed
Vinay <Vinay.Misra@kyndryl.com>
2025-07-21 12:16:08 +0200

    chaning the port details

--- app.py
+++ app.py
@@ -2,33 +2,18 @@
 import time
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.resources import Resource
-resource = Resource.create({
-    "service.name": "py-app",
-    "service.version": "1.0.0",
-    "deployment.environment": "dev"
-})
-
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-from opentelemetry.sdk.resources import Resource
-
-resource= Resource.create({
-"service.name": "py-app",
-"service.version": "1.0.0",
-"deployment.environment": "dev"
-
-})
-
-trace.set_tracer_provider(TracerProvider(resource=resource))
+trace.set_tracer_provider(TracerProvider())
 tracer = trace.get_tracer(__name__)
 
 otlp_exporter = OTLPSpanExporter(
-    endpoint="http://elastic-apm-server.elastic-system.svc:8200/v1/traces",  # no http:// prefix for gRPC
-    headers={"Authorization": "Bearer Wl9sQ0xKZ0JWNlJDY3RrRTlvY0U6cWZ4X0lxX0dwQXE2TGptdkFNWEpnQQ=="},
+    endpoint="elastic-apm-server.elastic-system.svc:8200",  # no http:// prefix for gRPC
+    headers={"authorization": "Bearer N3Rha0daZ0J5Z0RILVBja1d1Z3Q6U1NYU0RHU3B0RXYwNUowdTY3RTVldw=="},
+    insecure=True  # if your APM server does not use TLS
 )
 print("Using OTLP endpoint:", otlp_exporter._endpoint)
 
@@ -37,14 +22,7 @@ trace.get_tracer_provider().add_span_processor(
 )
 
 with tracer.start_as_current_span("example-span"):
-    print("Sending trace to Elastic APM Server directly via OTLP http")
-
-
-with tracer.start_as_current_span("example-transaction", kind=trace.SpanKind.SERVER) as span:
-    span.set_attribute("http.method", "GET")
-    span.set_attribute("http.url", "/example")
-    print("Sending trace to Elastic APM Server directly via OTLP http")
-
+    print("Sending trace to Elastic APM Server directly via OTLP gRPC")
 
 # Keep the app alive
 while True:
