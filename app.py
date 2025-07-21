1c3b360bcb98ba2910c7c2443440642b92a8e738
Vinay <Vinay.Misra@kyndryl.com>
2025-07-21 10:43:55 +0200

    chaning the port details

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import time
 from opentelemetry import trace
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -14,7 +13,6 @@ tracer = trace.get_tracer(__name__)
 otlp_exporter = OTLPSpanExporter(
     endpoint="http://elastic-apm-server.elastic-system.svc:8200",  # no http:// prefix for gRPC
     headers={"Authorization": "Bearer N3Rha0daZ0J5Z0RILVBja1d1Z3Q6U1NYU0RHU3B0RXYwNUowdTY3RTVldw=="},
-    insecure=True  # if your APM server does not use TLS
 )
 print("Using OTLP endpoint:", otlp_exporter._endpoint)
 
@@ -23,7 +21,7 @@ trace.get_tracer_provider().add_span_processor(
 )
 
 with tracer.start_as_current_span("example-span"):
-    print("Sending trace to Elastic APM Server directly via OTLP gRPC")
+    print("Sending trace to Elastic APM Server directly via OTLP http")
 
 # Keep the app alive
 while True:
